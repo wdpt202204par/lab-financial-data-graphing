1e20eb462e90e69f83ebadad89b08154401d1894
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-07-16 12:07:27 +0200

    iteration 2

---
+++ public/js/financial-data.js
@@ -0,0 +1,36 @@
+axios({
+    method: 'GET',
+    url: 'http://api.coindesk.com/v1/bpi/historical/close.json',
+    params: {start : '2021-07-10', end :'2022-07-10'}
+})
+
+    .then(response => {
+      console.log("We have datas")
+    })
+    .catch(err => {
+      console.log("Miss of datas")
+    });
+
+ 
+    function printTheChart(stockData) {
+        const dailyData = stockData['bpi'];
+       
+        const stockValues = Object.keys(dailyData);
+        const stockPrices = stockValues.map(date => dailyData[date][0]);
+       
+        const ctx = document.querySelector('my-chart').getContext('2d');
+        const chart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: stockValues,
+            datasets: [
+              {
+                label: 'Bitcoin Volatility',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: stockPrices
+              }
+            ]
+          }
+        }); // closes chart = new Chart()
+      } // closes printTheChart()
